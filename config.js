3b067bed20e923fbc0fff2df55bca1d5e78f9a3f
Max <sonthaya.nt@hotmail.com>
2021-12-17 00:16:56 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'kbide-farm1-handysense',
+    platform : 'arduino-esp32',
+    title : 'Farm1 x HandySense',
+    description : 'กล่องควบคุมฟาร์มระบบ IoT ส่งค่าขึ้น HandySense',
+    author : 'ArtronShop',
+    website : 'https://farm1.tech',
+    email : 'contact@atronshop.co.th',
+    git : 'https://github.com/Farm1-Tech/kbide-farm1-handysense/',
+    image : '/static/display.jpg',
+    version : '1.0.2'
+};
