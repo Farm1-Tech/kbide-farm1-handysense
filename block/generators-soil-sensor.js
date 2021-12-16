3fce5596605919cf68ad06cad6260afbed331f08
Max <sonthaya.nt@hotmail.com>
2021-12-16 23:36:19 +0700

    Add Capacitive Soil Moisture Sensor V1.2 work

--- block/generators-soil-sensor.js
+++ block/generators-soil-sensor.js
@@ -2,7 +2,7 @@ module.exports = function (Blockly) {
     "use strict";
 
     Blockly.JavaScript['capacitive_soil_moisture_sensor_v1.2_soil'] = function (block) {
-        var code = '#EXTINC#include <HandySenseXY_MD02.h>#END' + 'handySenseCapacitiveSoilMoistureSensorV1SoilRead';
+        var code = '#EXTINC#include <HandySenseCapacitiveSoilMoistureSensorV1_2.h>#END' + 'handySenseCapacitiveSoilMoistureSensorV1SoilRead';
 
         return [code, Blockly.JavaScript.ORDER_NONE];
     };
