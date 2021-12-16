d778a041ddef337ed2456168f9e8925e1e5f52ce
Max <sonthaya.nt@hotmail.com>
2021-12-16 18:41:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     author : 'ArtronShop',
     website : 'https://farm1.tech',
     email : 'contact@atronshop.co.th',
-    git : 'https://github.com/ArtronShop/kbide-farm1-handysense/',
+    git : 'https://github.com/Farm1-Tech/kbide-farm1-handysense/',
     image : '/static/display.jpg',
     version : '1.0.0'
 };
